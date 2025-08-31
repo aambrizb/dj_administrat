b035b906210e69f045d5e65a4a4ea2987edba59b
LizbethGuadalup <1627875x@umich.mx>
2025-08-30 21:24:18 -0600

    Se sube catalogo cliente y tipo credito

--- catalogo/admin.py
+++ catalogo/admin.py
@@ -4,3 +4,10 @@ from django.contrib import admin
 # Register your models here.
 class tipo_documentoAdmin(CustomModelAdmin):
   list_display = ('id','nombre','activo',)
+
+class clienteAdmin(CustomModelAdmin):
+  list_display = ('id','nombre','activo',)
+
+class tipo_creditoAdmin(CustomModelAdmin):
+  list_display = ('id','nombre','activo',)
+
--- catalogo/models.py
+++ catalogo/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class tipo_documento(models.Model):
@@ -7,3 +8,38 @@ class tipo_documento(models.Model):
 
   def __str__(self):
     return self.nombre
+
+class cliente(models.Model):
+  nombre = models.CharField(max_length=120)
+  apellido_paterno = models.CharField(max_length=120)
+  apellido_materno = models.CharField(max_length=120)
+  telefono = models.CharField(max_length=120)
+  observaciones = models.TextField(max_length=255)
+  direccion= models.ForeignKey("direccion", blank=False, null=True, on_delete=models.PROTECT)
+  fecha_registro = models.DateTimeField(default=timezone.localtime)
+  activo = models.BooleanField(default=True)
+
+  def __str__(self):
+    return self.nombre
+  
+class direccion(models.Model):
+  calle = models.CharField(max_length=120)
+  numero_exterior = models.CharField(max_length=120)
+  numero_interior = models.CharField(max_length=120)
+  colonia = models.CharField(max_length=120)
+  codigo_postal = models.CharField(max_length=120)
+  ciudad = models.CharField(max_length=120)
+  estado = models.CharField(max_length=120)
+  
+class tipo_credito(models.Model):
+  nombre = models.CharField(max_length=120)
+  meses = models.FloatField()
+  activo = models.BooleanField(default=True)
+
+  def __str__(self):
+    return self.nombre
+
+class tipo_credito_ciclo(models.Model):
+  tipo_credito = models.ForeignKey("tipo_credito", blank=False, null=False, on_delete=models.PROTECT)
+  ciclo = models.IntegerField()
+  tasa_interes = models.FloatField()
